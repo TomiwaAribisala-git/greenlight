a76e91ee00485c83670d8194392a8dbe672ac6e1
Tomiwa <tomiwaaribisala@gmail.com>
2024-05-29 18:17:17 +0100

    greenlight

---
+++ internal/jsonlog/jsonlog.go
@@ -0,0 +1,111 @@
+// Structured JSON Log Entries: for applications which do a lot of logging
+// Third Party Logger: Zerolog; for structured logging in JSON (and other) formats
+
+package jsonlog
+
+import (
+	"encoding/json"
+	"io"
+	"os"
+	"runtime/debug"
+	"sync"
+	"time"
+)
+
+type Level int8
+
+const (
+	LevelInfo  Level = iota // Has the value 0.
+	LevelError              // Has the value 1.
+	LevelFatal              // Has the value 2.
+	LevelOff                // Has the value 3.
+)
+
+func (l Level) String() string {
+	switch l {
+	case LevelInfo:
+		return "INFO"
+	case LevelError:
+		return "ERROR"
+	case LevelFatal:
+		return "FATAL"
+	default:
+		return ""
+	}
+}
+
+type Logger struct {
+	out      io.Writer
+	minLevel Level
+	mu       sync.Mutex
+}
+
+// Return a new Logger instance which writes log entries at or above a minimum severity
+// level to a specific output destination.
+func New(out io.Writer, minLevel Level) *Logger {
+	return &Logger{
+		out:      out,
+		minLevel: minLevel,
+	}
+}
+
+func (L *Logger) PrintInfo(message string, properties map[string]string) {
+	L.print(LevelInfo, message, properties)
+}
+
+func (L *Logger) PrintError(err error, properties map[string]string) {
+	L.print(LevelError, err.Error(), properties)
+}
+
+func (L *Logger) PrintFatal(err error, properties map[string]string) {
+	L.print(LevelFatal, err.Error(), properties)
+	os.Exit(1) // For entries at the FATAL level, we also terminate the application
+}
+
+func (L *Logger) print(level Level, message string, properties map[string]string) (int, error) {
+	// If the severity level of the log entry is below the minimum severity for the
+	// logger, then return with no further action.
+	if level < L.minLevel {
+		return 0, nil
+	}
+
+	// Declare an anonymous struct holding the data for the log entry.
+	aux := struct {
+		Level      string            `json:"level"`
+		Time       string            `json:"time"`
+		Message    string            `json:"message"`
+		Properties map[string]string `json:"properties,omitempty"`
+		Trace      string            `json:"trace,omitempty"`
+	}{
+		Level:      level.String(),
+		Time:       time.Now().UTC().Format(time.RFC3339),
+		Message:    message,
+		Properties: properties,
+	}
+
+	// Include a stack trace for entries at the ERROR and FATAL levels.
+	if level >= LevelError {
+		aux.Trace = string(debug.Stack())
+	}
+
+	// Declare a line variable for holding the actual log entry text.
+	var line []byte
+
+	line, err := json.Marshal(aux)
+	if err != nil {
+		line = []byte(LevelError.String() + ": unable to marshal log message: " + err.Error())
+	}
+
+	L.mu.Lock()
+	defer L.mu.Unlock()
+
+	// Write the log entry followed by a newline.
+	return L.out.Write(append(line, '\n'))
+}
+
+// We also implement a Write() method on our Logger type so that it satisfies the
+// io.Writer interface. This writes a log entry at the ERROR level with no additional
+// properties.
+func (l *Logger) Write(message []byte) (n int, err error) {
+	return l.print(LevelError, string(message), nil)
+}
